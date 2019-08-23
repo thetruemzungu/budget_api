db6e9b99cdefba09e553329002a6ad1c8250303e
Ethan Latimer <ethan_t_latimer@homedepot.com>
2019-08-23 16:03:56 -0400

    cleaning it up to rethink it.

--- app/routes/month_routes.js
+++ app/routes/month_routes.js
@@ -1,24 +1,16 @@
 var db = require("../services/db_service")
 module.exports = (app) => {
-    app.post('/biweeklyBudget', (req, res) => {
-        console.log(req.body)
-        if(req.body.hasOwnProperty('firstHalf')){
-            db.save('firstHalf', req.body['firstHalf'])
-            res.send(req.body)
-        }
-        else if(req.body.hasOwnProperty('secondHalf')){
-            db.save('secondHalf', req.body['secondHalf'])
-            res.send(req.body)
-        }
-        else {
-            res.send({"message": "Not a valid key"})
-        }
+    app.post('/budget', (req, res) => {
+        db.save('str', req.body[''])
+        res.send(req.body)
     })
 
-    app.get('/months/:key', (req, res) => {
+    app.get('/budget', (req, res) => {
+
+    })
+    app.get('/month/:key', (req, res) => {
         db.retrieve(req.params.key)
         .then((result) => {
-            console.log("good stuff right here: " + result)
             res.send({"result": result})
         })
         .catch((err) => {
